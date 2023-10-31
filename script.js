1fbe19779be1949e64b72c8b4c49b1ab3f27f745
Andy-Ugarte <andy.ugarte.schirrmeister@gmail.com>
2023-10-31 17:19:01 -0600

    updated

--- script.js
+++ script.js
@@ -13,136 +13,161 @@
             - use your browsers console throughout testing.
 */
 
-export const gameDetails = {   
-    title: 'Forkington',
-    desc: 'Welcome to the world of restaurant work!!!',
-    author: 'Andy Ugarte',
-    cohort: 'PTSD-2023',
-    startingRoomDescription: 'What you see before you is the lobby of a Mexican restaurant, from here you can go to the kitchen',
-    playerCommands: [
-        // replace these with your games commands as needed
-        'enter', 'view', 'pickup', ]
-    // Commands are basic things that a player can do throughout the game besides possibly moving to another room. This line will populate on the footer of your game for players to reference. 
-    // This shouldn't be more than 6-8 different commands.
-}
+export const gameDetails = {
+  title: "Forkington",
+  desc: "Welcome to the world of restaurant work!!!",
+  author: "Andy Ugarte",
+  cohort: "PTSD-2023",
+  startingRoomDescription:
+    "What you see before you is the lobby of a Mexican restaurant, from here you can go to the kitchen",
+  playerCommands: [
+    // replace these with your games commands as needed
+    "enter",
+    "view",
+    "pickup",
+  ],
+  // Commands are basic things that a player can do throughout the game besides possibly moving to another room. This line will populate on the footer of your game for players to reference.
+  // This shouldn't be more than 6-8 different commands.
+};
 
 // Your code here
 // const items = [ "couch", "matches", "stove", "pot","fountain", "glass","toilet", "tissue"]
 
 class Item {
-    constructor(name, description, location, move) {
-      this.name = name;
-      this.description = description;
-      this.location = location;
-      this.move = move;
-    }
+  constructor(name, description, location, move) {
+    this.name = name;
+    this.description = description;
+    this.location = location;
+    this.move = move;
+  }
 }
 
-let couch = new Item ('couch', 'brown sofa', 'lobby', false);
+let couch = new Item("couch", "brown sofa", "lobby", false);
 console.log(couch);
 
-let matches = new Item ('matches','you picked up fire sticks','lobby',true);
+let matches = new Item("matches", "you picked up fire sticks", "lobby", true);
 console.log(matches);
 
-let stove = new Item ('stove','cooktop', 'kitchen', false);
+let stove = new Item("stove", "cooktop", "kitchen", false);
 console.log(stove);
 
-let pot = new Item ('pot','you picked up a pot','kitchen', true);
+let pot = new Item("pot", "you picked up a pot", "kitchen", true);
 console.log(pot);
 
-let fountain = new Item ('fountain', 'soft drink dispenser', 'busCloset', false);
+let fountain = new Item("fountain", "soft drink dispenser", "busCloset", false);
 console.log(fountain);
 
-let glass = new Item ('glass', 'you picked up a cup', 'busCloset', true);
+let glass = new Item("glass", "you picked up a cup", "busCloset", true);
 console.log(glass);
 
-let toilet = new Item ('toilet', 'el water', 'bathroom', false);
+let toilet = new Item("toilet", "el water", "bathroom", false);
 console.log(toilet);
 
-let tissue = new Item ('tissue','you picked up a kleanex', 'bathroom', true);
+let tissue = new Item("tissue", "you picked up a kleanex", "bathroom", true);
 console.log(tissue);
 
 const items = {
-    // movable items
-    matches: matches,
-    pot: pot,
-    cup: cup,
-    tissue: tissue,
-    // non move items
-    couch: couch,
-    stove: stove,
-    fountain: fountain,
-    toilet: toilet
-} 
-
+  // movable items
+  matches: matches,
+  pot: pot,
+  glass: glass,
+  tissue: tissue,
+  // non move items
+  couch: couch,
+  stove: stove,
+  fountain: fountain,
+  toilet: toilet,
+};
 
 class Location {
-    constructor(avaibleExits, description, accessItems) {
-      this.avaibleExits = avaibleExits;
-      this.description = description;
-      this.accessItems = accessItems;
-    }
+  constructor(avaibleExits, description, accessItems) {
+    this.avaibleExits = avaibleExits;
+    this.description = description;
+    this.accessItems = accessItems;
+  }
 }
 
-let lobby = new Location (['kitchen'], 'host are front of house, from here you can go to the kitchen',['matches', 'couch'])
+let lobby = new Location(
+  ["kitchen"],
+  "host are front of house, from here you can go to the kitchen",
+  ["matches", "couch"]
+);
 console.log(lobby);
 
-let kitchen = new Location (['busCloset' , 'lobby'], 'a place where one cooks, from here you can go to the busCloset', ['pot','stove'])
+let kitchen = new Location(
+  ["busCloset", "lobby"],
+  "a place where one cooks, from here you can go to the busCloset",
+  ["pot", "stove"]
+);
 console.log(kitchen);
 
-let busCloset = new Location (['bathroom', 'kitchen'], 'a closet with basic restaurant supplies, from here you can go to the bathroom', ['cup','fountain'])
+let busCloset = new Location(
+  ["bathroom", "kitchen"],
+  "a closet with basic restaurant supplies, from here you can go to the bathroom",
+  ["cup", "fountain"]
+);
 console.log(busCloset);
 
-let bathroom = new Location (['window'], 'a room with toilets and sinks, from here you can leave from the window', ['tissue','toilet']);
+let bathroom = new Location(
+  ["window"],
+  "a room with toilets and sinks, from here you can leave from the window",
+  ["tissue", "toilet"]
+);
 console.log(bathroom);
 
-let window = new Location (['bathroom'], 'an exit, that leads you to bathroom' ) 
+let window = new Location(["bathroom"], "an exit, that leads you to bathroom");
 
 const locations = {
-    lobby: lobby , 
-    kitchen: kitchen,
-    busCloset: busCloset,
-    bathroom: bathroom,
-    window: window,
-    
+  lobby: lobby,
+  kitchen: kitchen,
+  busCloset: busCloset,
+  bathroom: bathroom,
+  window: window,
+};
+console.log(locations.lobby);
 
+let currentLocation = lobby;
 
-  }
-  console.log(locations.lobby);
-
-
-let currentLocation = lobby 
+let playerInventory = [];
 
 export const domDisplay = (playerInput) => {
-    // look up.split "method" ()
-    // "action" "target"
-    //write IF statmnts. 
+  // look up.split "method" ()
+  // "action" "target"
+  //write IF statmnts.
 
-let input = playerInput.split(" ")
-    console.log(input);
-let command = input[0]
+  let input = playerInput.split(" ");
+  console.log(input);
+  let command = input[0];
 
-let target = input[1]
+  let target = input[1];
 
-if(command === 'enter' && currentLocation.avaibleExits.includes(target)) {
-    currentLocation = locations[target] //locations.kitchen
-    return currentLocation.description
-} else if (command === 'pickup' && currentLocation.accessItems.includes(target)) {
-    //currentAccessItems = locations 
+  if (command === "enter" && currentLocation.avaibleExits.includes(target)) {
+    currentLocation = locations[target]; //locations.kitchen
+    return currentLocation.description;
+  } else if (
+    command === "pickup" &&
+    currentLocation.accessItems.includes(target)
+  ) {
+    //currentAccessItems = locations
     // have a value for item looking will be tager target, variable set current item, so if current ittem is = true or = false.
     // if else (maybe inside else if. )
-    return items[target].description
-    console.log('this works');
-    //code picking up object 
-}
+    if (items[target].move === true) {
+      playerInventory.push(items[target].name);
+    } else {
+      return `${items[target].name} not able to be moved`;
+    }
 
+    return items[target].description;
+    console.log("this works");
+    //code picking up object
+  } else if (command === "view" && target === "inventory") {
+    return playerInventory;
+    //console.log(playerInventory)
+  } else {
+    return "I don't recognize that command";
+  }
 
-
-
-
-
-
-    /* 
+  /* 
         TODO: for students
         - This function must return a string. 
         - This will be the information that is displayed within the browsers game interface above the users input field.
@@ -173,5 +198,5 @@ if(command === 'enter' && currentLocation.avaibleExits.includes(target)) {
                     - What is the process of picking up an item exactly? ex: Look. Pick from a list of items. Put into players list of items... 
     */
 
-    // Your code here
-} 
+  // Your code here
+};
